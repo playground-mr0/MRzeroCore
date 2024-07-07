5b6008cec873d78e0db2df3b9a4a89e432fb76bd
playground-mr0 <playgroundmr0@gmail.com>
2024-07-07 21:08:34 +0200

    Update Update_Cargo_version.sh

--- Update_Cargo_version.sh
+++ Update_Cargo_version.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+chmod +x Update_Cargo_version.sh
+
 # Get current version with commit hash and date
 VERSION=$(git describe --tags --dirty --always --long --abbrev=7)
 
 # Update Cargo.toml with the new version
 sed -i "s/^version = \".*\"$/version = \"$VERSION\"/" Cargo.toml
+
+# Update Cargo.lock with the new version (optional)
+cargo update -p PlaygroundMR01 --precise "$VERSION"  # Replace 'your-package-name' with your actual package name
